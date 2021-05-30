eaf98a551d8649f77254a5b633a65270b0bc0ad9
lorde78 <62546853+lorde78@users.noreply.github.com>
2021-05-30 12:43:23 +0200

    contact

--- js/panier.js
+++ js/panier.js
@@ -4,6 +4,8 @@ console.log(bouton);
 const confirma = document.querySelector('.remerciement')
 const wrapper = document.querySelector('.small-container')
 
+
+
 bouton.addEventListener('click', confirmation);
 
 function confirmation(){
@@ -11,6 +13,13 @@ function confirmation(){
     confirma.classList.add('actif')
 }
 
+var removeBtn = document.querySelectorAll('.danger')
+console.log(removeBtn);
 
-
-
+for (var i = 0 < removeBtn.length; i++){
+    var button = removeBtn[i]
+    button.addEventListener('click', function(event){
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+    })
+}
