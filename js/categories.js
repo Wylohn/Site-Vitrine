edbbdfe11c1e5999f77611fee7c88ed5a281ff1d
Wylohn <wylohn.tribout@gmail.com>
2021-05-30 21:42:31 +0200

    carte complète

--- js/categories.js
+++ js/categories.js
@@ -2,5 +2,21 @@ var categories = [
     {
         "id": 1,
         "name": "Sushi"
+    },
+    {
+        "id": 2,
+        "name" : "Maki"
+    },
+    {
+        "id": 3,
+        "name": "California"
+    },
+    {
+        "id": 4,
+        "name": "Sashimi"
+    },
+    {
+        "id": 5,
+        "name": "Rolls"
     }
 ]
